0ed21a185045a52e243b2485d35d0368e1c2e0bc
Ia Kurnia <iakurnia26@gmail.com>
2021-12-21 22:32:45 +0700

    Algorithm RSA For File WIth RSA Only File Text

--- src/com/company/algorithmrsa/file/MainControllerFile.java
+++ src/com/company/algorithmrsa/file/MainControllerFile.java
@@ -4,14 +4,28 @@ import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class MainControllerFile {
-    public static void main(String[] args) throws NoSuchPaddingException, IOException, NoSuchAlgorithmException, InvalidKeyException {
+    public static void main(String[] args) throws NoSuchPaddingException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException {
         System.out.println("Hello World");
         RSAOnlyFile r=new RSAOnlyFile();
 //        r.ecnrypFileAESOnly();  coment cause done encrypted
-        r.decryptedAesOnly();
+//        r.decryptedAesOnly(); comment cause done decrypted
 
+        /**
+         * encrypt decrypt with algoritm RSA
+         * RSA is Rivest-Shamir-Adleman (Cryptosystem)
+         * #1 create generator key
+         * #2 create encrypt with generate private key
+         * #3 create decrypt with generate public key
+         * #4 run execute Script
+         * #5 Done comit and push
+         * */
+
+        r.genKey();
+        r.encryptedRSAOnly();
+        r.dencryptedRSAOnly();
 
     }
 }
--- src/com/company/algorithmrsa/file/RSAOnlyFile.java
+++ src/com/company/algorithmrsa/file/RSAOnlyFile.java
@@ -8,10 +8,79 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class RSAOnlyFile {
+    public void genKey() throws NoSuchAlgorithmException, IOException {
+        String fileBase = "genKey";
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        KeyPair kp = kpg.generateKeyPair();
+        try (FileOutputStream fo = new FileOutputStream(fileBase + ".private")) {
+            fo.write(kp.getPrivate().getEncoded());
+        }
+        try (FileOutputStream fo = new FileOutputStream(fileBase + ".public")) {
+            fo.write(kp.getPublic().getEncoded());
+        }
+        System.out.println("Done Generate Key");
+    }
+
+    public void encryptedRSAOnly() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException {
+        String privateKey = "genKey.private";
+        byte[] bytes = Files.readAllBytes(Paths.get(privateKey));
+        PKCS8EncodedKeySpec ksc = new PKCS8EncodedKeySpec(bytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PrivateKey pk = kf.generatePrivate(ksc);
+
+        Cipher c = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        c.init(Cipher.ENCRYPT_MODE, pk);
+
+        FileInputStream file = new FileInputStream("DataFile/testFile.txt");
+        FileOutputStream fo = new FileOutputStream("DataFile/EncryptedTextFile.txt");
+
+        CipherOutputStream cos = new CipherOutputStream(fo, c);
+        byte[] b = new byte[1024];
+        int read;
+        while ((read = file.read(b)) != -1) {
+            cos.write(b, 0, read);
+        }
+        System.out.println("Encrypted Done");
+        file.close();
+        fo.flush();
+        cos.close();
+    }
+
+    public  void dencryptedRSAOnly() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException {
+        String publicKey="genKey.public";
+        byte[] bytes=Files.readAllBytes(Paths.get(publicKey));
+        X509EncodedKeySpec ks=new X509EncodedKeySpec(bytes);
+        KeyFactory kf=KeyFactory.getInstance("RSA");
+        PublicKey pk=kf.generatePublic(ks);
+
+        Cipher c=Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        c.init(Cipher.DECRYPT_MODE,pk);
+
+        FileInputStream file=new FileInputStream("DataFile/EncryptedTextFile.txt");
+        FileOutputStream fo=new FileOutputStream("DataFile/DecryptedTextFile.txt");
+        CipherOutputStream cos=new CipherOutputStream(fo,c);
+        byte[] b=new byte[1024];
+        int read;
+        while((read = file.read(b))!=-1){
+            cos.write(b,0,read);
+        }
+        System.out.println("Decrypted Done");
+        file.close();
+        fo.flush();
+        cos.close();
+    }
+
+
     public void ecnrypFileAESOnly() throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
         /**
          * AES is Advance Encryption Standar
@@ -35,7 +104,7 @@ public class RSAOnlyFile {
 
     }
 
-    public  void decryptedAesOnly() throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+    public void decryptedAesOnly() throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
         FileInputStream file = new FileInputStream("DataFile/EncryptedJava.png");
         FileOutputStream fo = new FileOutputStream("DataFile/DencryptedJava.png");
         byte k[] = "Cool2116NiTh5252".getBytes(); // default value
@@ -53,4 +122,6 @@ public class RSAOnlyFile {
         fo.flush();
         cos.close();
     }
+
+
 }
